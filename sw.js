74e89a98c8c10444d8ac3866545e4b42b0e32a5b
jorgefch-cyber <jorgefabre.business@gmail.com>
2026-08-18 01:26:50 -0600

    sw: v15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* JobSwipe service worker: cachea el caparazón, nunca los datos. */
-const CACHE = 'jobswipe-v14';
+const CACHE = 'jobswipe-v15';
 const SHELL = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
